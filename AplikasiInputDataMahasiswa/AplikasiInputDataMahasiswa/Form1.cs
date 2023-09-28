e8cd065e63cc326edc815b52be4c11365ae813d4
pajjeee <127712783+pajjeee@users.noreply.github.com>
2023-09-28 19:21:53 +0700

    Add files via upload

---
+++ AplikasiInputDataMahasiswa/AplikasiInputDataMahasiswa/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiInputDataMahasiswa
+{
+    public partial class Form1 : Form
+    {
+        private List<Mahasiswa> list = new List<Mahasiswa>();
+        public Form1()
+        {
+            InitializeComponent();
+            InisialisasiListView();
+            ResetForm();
+      
+            
+            
+        }
+        private void InisialisasiListView()
+        {
+            lvwMahasiswa.View = View.Details;
+            lvwMahasiswa.FullRowSelect = true;
+            lvwMahasiswa.GridLines = true;
+            lvwMahasiswa.Columns.Add("No.", 30, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nim", 91, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nama", 200, HorizontalAlignment.Left);
+            lvwMahasiswa.Columns.Add("Kelas", 70, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nilai", 50, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nilai Huruf", 70, HorizontalAlignment.Center);
+        }
+        private void ResetForm()
+        {
+            txtNim.Clear();
+            txtNama.Clear();
+            txtKelas.Clear();
+            txtNilai.Text = "0";
+            txtNim.Focus();
+        }
+       
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            // membuat objek mahasiswa 
+            Mahasiswa mhs = new Mahasiswa();
+            // set nilai masing-masing propertynya
+            // berdasarkan inputan yang ada di form
+            mhs.Nim = txtNim.Text;
+            mhs.Nama = txtNama.Text;
+            mhs.Kelas = txtKelas.Text;
+            mhs.Nilai = int.Parse(txtNilai.Text);
+            // tambahkan objek mahasiswa ke dalam collection
+            list.Add(mhs);
+            var msg = "Data mahasiswa berhasil disimpan.";
+            // tampilkan dialog informasi
+            MessageBox.Show(msg, "Informasi", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+            // reset form input
+            ResetForm();
+        }
+        private void btnTampilkanData_Click(object sender, EventArgs e)
+        {
+            // kosongkan data listview
+            lvwMahasiswa.Items.Clear();
+            // lakukan perulangan untuk menampilkan data mahasiswa ke listview
+            foreach (var mhs in list)
+            {
+                var noUrut = lvwMahasiswa.Items.Count + 1;
+                var item = new ListViewItem(noUrut.ToString());
+                item.SubItems.Add(mhs.Nim);
+                item.SubItems.Add(mhs.Nama);
+                item.SubItems.Add(mhs.Kelas);
+                item.SubItems.Add(mhs.Nilai.ToString());
+                item.SubItems.Add(mhs.nilaihuruf);
+                lvwMahasiswa.Items.Add(item);
+            }
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNilai_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNilai_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+       
+    }
+}
---
+++ AplikasiInputDataMahasiswa/AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    class Mahasiswa
+    {
+        public String Nim;
+        public String Nama;
+        public String Kelas;
+        private int nilai;
+        public String nilaihuruf;
+
+        public int Nilai
+        {
+            get { return nilai; }
+            set
+            {
+                int val = value;
+                nilai = val;
+                if (val > 81)
+                {
+                    nilaihuruf = "A";
+                }
+                else if (val > 61)
+                {
+                    nilaihuruf = "B";
+                }
+                else if (val > 41)
+                {
+                    nilaihuruf = "C";
+                }
+                else if (val > 21)
+                {
+                    nilaihuruf = "D";
+                }
+                else if (val > 0)
+                {
+                    nilaihuruf = "E";
+                }
+            }
+        }
+    }
+}
